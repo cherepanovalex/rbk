c9320eda9dbebf26611f2e432b945f51e372441d
alex cherepanov <a.cherepanov@olimp.kz>
2019-10-17 20:01:22 +0600

    pages-forms-bottom

--- src/js/script.js
+++ src/js/script.js
@@ -2,16 +2,32 @@ $(function () {
 
     // **слайдер
 
-    // var swiper = new Swiper('.swiper-container', {
-    //     direction: 'vertical',
-    //     pagination: {
-    //         el: '.swiper-pagination',
-    //         clickable: true,
-    //     },
-    // });
+
+
+    //scroll to element
+
+
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.stop().animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+
+    //lang
+    var selectorLang = '.lang a';
+
+    $(selectorLang).on('click', function(){
+        $(selectorLang).removeClass('active');
+        $(this).addClass('active');
+    });
 
 
 
+
+//mask
+    $('.tel').inputmask({"mask": "+7(999) 999-99-99"});
 
 
 });
